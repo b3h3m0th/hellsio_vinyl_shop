7247bad65b76a4b194d3efd11481d6bc37344cb5
Behemoth <simonostini@gmail.com>
2021-05-01 18:06:55 +0200

    ✨ update

--- src/pages/Admin/Products/Product/Product.tsx
+++ src/pages/Admin/Products/Product/Product.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Product.scss";
 
 interface ProductProps {
@@ -15,8 +15,23 @@ const Product: React.FC<ProductProps> = ({
   price,
 }: ProductProps) => {
   const [editable, setEditable] = useState<boolean>(false);
+  const nameRef = useRef<HTMLDivElement>(null);
+  const artistRef = useRef<HTMLDivElement>(null);
+  const priceRef = useRef<HTMLElement>(null);
 
-  const save = () => {};
+  const [updatedValues, setUpdatedValues] = useState<{
+    name: string;
+    artist: string;
+    price: number;
+  }>({
+    name: name,
+    artist: artist,
+    price: +price,
+  });
+
+  const save = () => {
+    setEditable(false);
+  };
   return (
     <div className="admin-product">
       <img
@@ -25,19 +40,40 @@ const Product: React.FC<ProductProps> = ({
         className="admin-product__img"
       />
       <div className="admin-product__header">
-        <div className="admin-product__header__title">{name}</div>
-        <div className="admin-product__header__artist">{artist}</div>
+        <div className="admin-product__header__title" ref={nameRef}>
+          {name}
+        </div>
+        <div className="admin-product__header__artist" ref={artistRef}>
+          {artist}
+        </div>
       </div>
       <div className="admin-product__price">
         <div className="admin-product__price__price">
-          $<span contentEditable={editable}>{price}</span>
+          $
+          <span
+            contentEditable={editable}
+            onInput={(e: React.FormEvent<HTMLElement>) =>
+              setUpdatedValues({
+                name: updatedValues.name,
+                artist: updatedValues.artist,
+                price: +e.currentTarget.innerText,
+              })
+            }
+            ref={priceRef}
+            suppressContentEditableWarning={true}
+          >
+            {price}
+          </span>
         </div>
         <span className="admin-product__price__per-item">per item</span>
       </div>
       <div className="admin-product__actions">
         <span
           className="admin-product__actions__edit"
-          onClick={() => setEditable((prev: boolean) => !prev)}
+          onClick={() => {
+            setEditable((prev: boolean) => !prev);
+            editable ? (priceRef.current!.innerText = price) : void 0;
+          }}
         >
           {editable ? "Cancel" : "Edit"}
         </span>
