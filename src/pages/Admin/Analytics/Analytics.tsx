cada55df70d6302334e00a1d436b43dddc526b21
Behemoth <simonostini@gmail.com>
2021-05-02 16:45:20 +0200

    🚑 update

--- src/pages/Admin/Analytics/Analytics.tsx
+++ src/pages/Admin/Analytics/Analytics.tsx
@@ -1,10 +1,191 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Analytics.scss";
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryTheme,
+  VictoryScatter,
+  VictoryCursorContainer,
+} from "victory";
+import { fetchTopCustomers, fetchTopSellingAlbums } from "./fetchData";
+import Title from "../../../components/Title/Title";
 
 const Analytics: React.FC = () => {
+  const [analyticsData, setAnalyticsData] = useState<{
+    topCustomers: Array<any>;
+    topSellingAlbums: Array<any>;
+  }>();
+
+  useEffect(() => {
+    (async () => {
+      const topCustomers = await fetchTopCustomers(5);
+      const topSellingAlbums = await fetchTopSellingAlbums(5);
+
+      setAnalyticsData({
+        topCustomers: topCustomers as any,
+        topSellingAlbums: topSellingAlbums as any,
+      });
+    })();
+  }, []);
+
+  console.log(analyticsData);
+
   return (
     <div className="admin-analytics">
-      <div>Analytics</div>
+      <div className="admin-analytics__wrapper">
+        <Title link={`admin/analytics`} title="Anayltics" />
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top Customers by Orders</div>
+          <div>
+            <VictoryChart
+              theme={VictoryTheme.material}
+              domainPadding={50}
+              width={1000}
+            >
+              <VictoryBar
+                style={{
+                  data: { fill: "var(--color-red)", width: 50 },
+                }}
+                data={[...(analyticsData?.topCustomers || [])]?.map(
+                  (c: any) => {
+                    return { x: c.email, y: 0, y0: c.invoice_count };
+                  }
+                )}
+                animate={{ onLoad: { duration: 0 } }}
+              />
+              <VictoryScatter
+                containerComponent={
+                  <VictoryCursorContainer
+                    cursorLabel={({ x, y }) =>
+                      `${x.toPrecision(2)}, ${y.toPrecision(2)}`
+                    }
+                  />
+                }
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart
+              theme={VictoryTheme.material}
+              width={1200}
+              domainPadding={50}
+            >
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)", width: 50 } }}
+                data={[...(analyticsData?.topSellingAlbums || [])]?.map(
+                  (p: any) => {
+                    return { x: p.code, y: 0, y0: p.sold_count };
+                  }
+                )}
+                animate={{ onLoad: { duration: 0 } }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)" } }}
+                data={[
+                  { x: "Peter", y: 0, y0: 5 },
+                  { x: "Simon", y: 0, y0: 3 },
+                  { x: "Paul", y: 0, y0: 2 },
+                  { x: "Jonas", y: 0, y0: 8 },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)" } }}
+                data={[
+                  { x: "Peter", y: 0, y0: 5 },
+                  { x: "Simon", y: 0, y0: 3 },
+                  { x: "Paul", y: 0, y0: 2 },
+                  { x: "Jonas", y: 0, y0: 8 },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)" } }}
+                data={[
+                  { x: "Peter", y: 0, y0: 5 },
+                  { x: "Simon", y: 0, y0: 3 },
+                  { x: "Paul", y: 0, y0: 2 },
+                  { x: "Jonas", y: 0, y0: 8 },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)" } }}
+                data={[
+                  { x: "Peter", y: 0, y0: 5 },
+                  { x: "Simon", y: 0, y0: 3 },
+                  { x: "Paul", y: 0, y0: 2 },
+                  { x: "Jonas", y: 0, y0: 8 },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="admin-analytics__wrapper__chart">
+          <div>Top selling Albums</div>
+          <div>
+            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+              <VictoryBar
+                style={{ data: { fill: "var(--color-red)" } }}
+                data={[
+                  { x: "Peter", y: 0, y0: 5 },
+                  { x: "Simon", y: 0, y0: 3 },
+                  { x: "Paul", y: 0, y0: 2 },
+                  { x: "Jonas", y: 0, y0: 8 },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/Admin/Analytics/fetchData.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+import {
+  getAdminAccessToken,
+  getAdminRefreshToken,
+  setAdminAccessToken,
+} from "../../../authorization/token";
+
+export const fetchTopCustomers = async (count: number): Promise<void> => {
+  const accessToken = getAdminAccessToken();
+  const refreshToken = getAdminRefreshToken();
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_BASE_API_URL}/admin/customers/top/${count}`,
+      { headers: { authorization: `Bearer ${accessToken}` } }
+    );
+    return response.data;
+  } catch (err) {
+    const tokenResponse = await axios.post(
+      `${`${process.env.REACT_APP_BASE_API_URL}/admin/token` || ""}`,
+      {
+        token: refreshToken,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setAdminAccessToken(tokenResponse.data.accessToken);
+    return await fetchTopCustomers(count);
+  }
+};
+
+export const fetchTopSellingAlbums = async (count: number): Promise<void> => {
+  const accessToken = getAdminAccessToken();
+  const refreshToken = getAdminRefreshToken();
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_BASE_API_URL}/admin/products/top/${count}`,
+      { headers: { authorization: `Bearer ${accessToken}` } }
+    );
+    return response.data;
+  } catch (err) {
+    const tokenResponse = await axios.post(
+      `${`${process.env.REACT_APP_BASE_API_URL}/admin/token` || ""}`,
+      {
+        token: refreshToken,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setAdminAccessToken(tokenResponse.data.accessToken);
+    return await fetchTopCustomers(count);
+  }
+};
