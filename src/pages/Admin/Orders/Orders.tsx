5a41db49bde36e5b7e36a8b426d115d6ce1e97cb
Behemoth <simonostini@gmail.com>
2021-05-05 14:40:07 +0200

    🚀 update

--- src/pages/Admin/Orders/Orders.tsx
+++ src/pages/Admin/Orders/Orders.tsx
@@ -5,7 +5,9 @@ import Loader from "../../../components/Loader/Loader";
 import Order from "./Order/Order";
 
 const Orders: React.FC = () => {
+  const itemsPerPage = 10 as const;
   const [orders, setOrders] = useState<any & {}>([]);
+  const [page, setPage] = useState<number>(0);
 
   useEffect(() => {
     (async () => {
@@ -14,19 +16,44 @@ const Orders: React.FC = () => {
   }, []);
 
   return (
-    <div className="admin-orders">
-      <div className="admin-orders__wrapper">
-        {orders ? (
-          Object.keys(orders)
-            .reverse()
-            .map((key, index) => {
-              return <Order key={index} invoiceline={orders[key]} />;
-            })
-        ) : (
-          <Loader>Loading</Loader>
-        )}
+    <>
+      <div className="admin-orders">
+        <div className="admin-orders__wrapper">
+          {orders ? (
+            <>
+              {Object.keys(orders)
+                .reverse()
+                .splice(itemsPerPage * page, itemsPerPage)
+                .map((key, index) => {
+                  return <Order key={index} invoiceline={orders[key]} />;
+                })}
+              <div className="admin-orders__wrapper__pagination">
+                <div>
+                  <b>Page</b>
+                  {[
+                    ...new Array(
+                      Math.ceil(Object.keys(orders).length / itemsPerPage)
+                    ),
+                  ].map((x: any, i: number) => {
+                    return (
+                      <span
+                        className="admin-orders__wrapper__pagination__page-link"
+                        key={i}
+                        onClick={() => setPage(i)}
+                      >
+                        {i + 1}
+                      </span>
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          ) : (
+            <Loader>Loading</Loader>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
