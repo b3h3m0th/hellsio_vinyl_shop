0c34f7d94d4903329bb0c5cbc531f2aa37dec00f
Behemoth <simonostini@gmail.com>
2021-05-12 09:17:34 +0200

    🐛 update

--- src/components/GenreList/GenreCheckBox/GenreCheckBox.tsx
+++ src/components/GenreList/GenreCheckBox/GenreCheckBox.tsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { CSSProperties } from "react";
 import "./GenreCheckBox.scss";
 
 interface GenreCheckBoxProps {
   label: string;
   checked: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  style?: CSSProperties;
 }
 
 const GenreCheckBox: React.FC<GenreCheckBoxProps> = ({
   label,
   checked,
   onChange,
+  style,
 }: GenreCheckBoxProps) => {
   return (
     <label
       className={`genre-checkbox__container ${
         checked ? "checkbox-checked" : "checkbox-not-checked"
       }`}
+      style={style && style}
     >
       <span className={`genre-checkbox__container__checkmark`}></span>
       {label}
--- src/pages/Admin/WebsiteContent/WebsiteContent.tsx
+++ src/pages/Admin/WebsiteContent/WebsiteContent.tsx
@@ -5,6 +5,7 @@ import { inject, observer } from "mobx-react";
 import { useEffect } from "react";
 import Title from "../../../components/Title/Title";
 import { useState } from "react";
+import GenreCheckBox from "../../../components/GenreList/GenreCheckBox/GenreCheckBox";
 
 interface WebsiteContentProps {
   redisStore?: RedisStore;
@@ -18,11 +19,13 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
     subtitle: string;
     maxProductAddToCartAmount: number;
     maxProductTotalOrderAmount: number;
+    radioMusicEnabled: boolean;
   }>({
     title: "",
     subtitle: "",
     maxProductAddToCartAmount: 0,
     maxProductTotalOrderAmount: 0,
+    radioMusicEnabled: true,
   });
 
   useEffect(() => {
@@ -35,11 +38,15 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
       const maxProductTotalOrderAmount = await redisStore?.getValue(
         "max-product-total-order-amount"
       );
+      const radioMusicEnabled = await redisStore?.getValue(
+        "radio-music-enabled"
+      );
       setData({
         title,
         subtitle,
         maxProductAddToCartAmount,
         maxProductTotalOrderAmount,
+        radioMusicEnabled: radioMusicEnabled as boolean,
       });
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -57,7 +64,7 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
         <Title link={`admin/website-content`} title="Website Settings" />
         <div className="admin-website-content__wrapper__setting">
           <span className="admin-website-content__wrapper__setting__header">
-            Hero Title:{" "}
+            Hero Title:
           </span>
           <input
             type="text"
@@ -77,7 +84,6 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
         </div>
         <div className="admin-website-content__wrapper__setting">
           <span className="admin-website-content__wrapper__setting__header">
-            {" "}
             Hero Subtitle
           </span>
           <input
@@ -98,7 +104,6 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
         </div>
         <div className="admin-website-content__wrapper__setting">
           <span className="admin-website-content__wrapper__setting__header">
-            {" "}
             Max. Product add to cart Amount
           </span>
           <input
@@ -124,7 +129,6 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
         </div>
         <div className="admin-website-content__wrapper__setting">
           <span className="admin-website-content__wrapper__setting__header">
-            {" "}
             Max. Product Total Order Amount
           </span>
           <input
@@ -142,6 +146,30 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({
               handleSave(
                 "max-product-total-order-amount",
                 data.maxProductTotalOrderAmount.toString()
+              )
+            }
+          >
+            Save
+          </span>
+        </div>
+        <div className="admin-website-content__wrapper__setting">
+          <span className="admin-website-content__wrapper__setting__header">
+            Radio Music Enabled
+          </span>
+          <GenreCheckBox
+            checked={data.radioMusicEnabled}
+            label=""
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setData({ ...data, radioMusicEnabled: e.target.checked });
+            }}
+            style={{ display: "inline-flex" }}
+          />
+          <span
+            className="admin-website-content__wrapper__setting__save"
+            onClick={() =>
+              handleSave(
+                "radio-music-enabled",
+                data.radioMusicEnabled.toString()
               )
             }
           >
